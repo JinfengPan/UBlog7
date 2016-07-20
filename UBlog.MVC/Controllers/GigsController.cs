568f13db9658aa7197f8d798c70e7a53f95bf1c7
patrickfly <flypatrickg@163.com>
2016-07-20 20:40:15 +0800

    d

---
+++ UBlog.MVC/Controllers/GigsController.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using UBlog.MVC.Models;
+using UBlog.MVC.ViewModels;
+
+namespace UBlog.MVC.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ UBlog.MVC/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UBlog.MVC.Models;
+
+namespace UBlog.MVC.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+
+    }
+}
